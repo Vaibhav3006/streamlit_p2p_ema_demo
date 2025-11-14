c994a20d742f6cefd3bc11f1494604d712e55b3c
Vaibhav Bhatia <vaibhavb2473@gmail.com>
2025-11-14 08:50:56 +0530

    Update 1_Ingestion_&_Extraction.py

--- pages/1_Ingestion_&_Extraction.py
+++ pages/1_Ingestion_&_Extraction.py
@@ -246,7 +246,7 @@ if process_btn:
     # active_dirs = str(Path("Data/incoming_sample").resolve())
     # output_dir = str(Path("Data/Output").resolve())
 
-    BASE_DIR = Path(__file__).resolve().parent  # folder containing this .py file
+    BASE_DIR = Path(__file__).resolve().parent.parent  # folder containing this .py file
     DATA_DIR = BASE_DIR / "Data"
 
     active_dirs = str(DATA_DIR / "incoming_sample")
